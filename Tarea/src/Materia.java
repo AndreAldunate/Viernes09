a4a7a9b9e20508c98e6691b3080feb53f5e8a0e0
Apita49 <jose.apa4996@gmail.com>
2017-06-09 08:31:21 -0400

    La clase Materia con getters y setters

--- Tarea/src/Materia.java
+++ Tarea/src/Materia.java
@@ -8,4 +8,29 @@ public class Materia {
 		return "Materia: " + nombre + " con el codigo: " + codigo
 				+ " en el curso: " + curso;
 	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public int getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(int nombre) {
+		this.nombre = nombre;
+	}
+
+	public Curso getCurso() {
+		return curso;
+	}
+
+	public void setCurso(Curso curso) {
+		this.curso = curso;
+	}
+
 }
