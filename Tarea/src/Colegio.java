5b24e31d3d5f44fa55ed85ebedb6770084d69dea
AndreAldunate <andre_aldunate@hotmail.com>
2017-06-09 07:51:36 -0400

    Add files via upload

---
+++ Tarea/src/Colegio.java
@@ -0,0 +1,14 @@
+
+public class Colegio {
+			
+	private String nombre;
+	private int alumnos;
+	private String direccion;
+	
+	public void mostrar(){
+		System.out.println("El colegio es:" + nombre);
+		System.out.println("Capacidad de alumnos: " + alumnos);
+		System.out.println("Su direccion es" + direccion);
+	}
+
+}
